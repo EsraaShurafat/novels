893906f4c90b37fa77174dd5889d05c71d180512
EsraaAlshurafat <>
2021-06-16 13:07:09 +0000

    adding jsapp

---
+++ app.js
@@ -0,0 +1,20 @@
+var utherName = prompt("what is the Auther name ?")
+
+alert("welcome to the best novels of 2020 website " )
+
+var novelname = prompt("what is the novel name ?")
+
+
+if(novelname == "the vanishing"){
+  document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.co.uk/Vanishing-Half-bestselling-author-Mothers/dp/0349701466'>The Vanishing Half</a>")
+}
+ else if(novelname == "children's bible")
+{document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.com/Childrens-Bible-Lydia-Millet/dp/1690592435'>A Children's Bible </a")
+ 
+ 
+
+
+}
+else{
+  alert("the novel doesnt here and thank for your visit.")
+}
