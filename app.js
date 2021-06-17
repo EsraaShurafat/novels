747cceb786d19ce4e52e6f73a1cbe04003c21d87
EsraaAlshurafat <>
2021-06-17 13:11:22 +0000

    while loop adding to js app

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-// var utherName = prompt("what is the Auther name ?")
+var userName = prompt("what is the your name ?")
 
-// alert("welcome to the best novels of 2020 website " + utherName  )
+alert("welcome to the best novels of 2020 website " + userName  )
 
-// var novelname = prompt("what is the novel name ?")
+var novelname = prompt("what is the novel name ?")
+
+while (novelname != "the vanishing" && novelname!= "children's bible") {
+
+  novelname = prompt(" pelase! try gain ?")
+
+}
 
 
-// if(novelname == "the vanishing"){
-//   document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.co.uk/Vanishing-Half-bestselling-author-Mothers/dp/0349701466'>The Vanishing Half</a>")
-// }
-//  else if(novelname == "children's bible")
-// {document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.com/Childrens-Bible-Lydia-Millet/dp/1690592435'>A Children's Bible </a")
+if(novelname == "the vanishing"){
+  document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.co.uk/Vanishing-Half-bestselling-author-Mothers/dp/0349701466'>The Vanishing Half</a>")
+}
+ else if(novelname == "children's bible")
+{document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.com/Childrens-Bible-Lydia-Millet/dp/1690592435'>A Children's Bible </a")
 
-// }
-// else{
-//   alert("the novel doesnt here and thank for your visit.")
-// }
+}
+else{
+  alert("the novel doesnt here and thank for your visit.")
+}
