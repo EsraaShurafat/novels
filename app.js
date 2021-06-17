b88fe38e4d1c2dffd2db6a687d09726739332934
EsraaAlshurafat <>
2021-06-17 11:42:16 +0000

    html file

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var utherName = prompt("what is the Auther name ?")
+// var utherName = prompt("what is the Auther name ?")
 
-alert("welcome to the best novels of 2020 website " + utherName  )
+// alert("welcome to the best novels of 2020 website " + utherName  )
 
-var novelname = prompt("what is the novel name ?")
+// var novelname = prompt("what is the novel name ?")
 
 
-if(novelname == "the vanishing"){
-  document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.co.uk/Vanishing-Half-bestselling-author-Mothers/dp/0349701466'>The Vanishing Half</a>")
-}
- else if(novelname == "children's bible")
-{document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.com/Childrens-Bible-Lydia-Millet/dp/1690592435'>A Children's Bible </a")
+// if(novelname == "the vanishing"){
+//   document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.co.uk/Vanishing-Half-bestselling-author-Mothers/dp/0349701466'>The Vanishing Half</a>")
+// }
+//  else if(novelname == "children's bible")
+// {document.write("<p>"+ "enjoy your reading"+"</p>"+"<a href='https://www.amazon.com/Childrens-Bible-Lydia-Millet/dp/1690592435'>A Children's Bible </a")
 
-}
-else{
-  alert("the novel doesnt here and thank for your visit.")
-}
+// }
+// else{
+//   alert("the novel doesnt here and thank for your visit.")
+// }
